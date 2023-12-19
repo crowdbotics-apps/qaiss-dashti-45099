ed8a295d2c2762e6951cc786d07c662ff7645d59
Crowdbotics <team@crowdbotics.com>
2023-12-19 13:49:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qaiss-dashti-45099",
+  url: "https://qaiss-dashti-45099.botics.co",
+  api: "https://qaiss-dashti-45099.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
